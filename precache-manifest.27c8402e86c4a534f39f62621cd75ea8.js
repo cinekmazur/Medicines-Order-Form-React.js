a3760a362f92da0d36fe5ae8b1390ad510ff998e
cinekmazur <cinek.mazur@gmail.com>
2020-06-11 00:31:01 +0200

    Updates

---
+++ precache-manifest.27c8402e86c4a534f39f62621cd75ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ff58f84d7bddc047f120a9e1aa8864",
+    "url": "/Medicines-Order-Form-React.js/index.html"
+  },
+  {
+    "revision": "e8839899f0b33f337057",
+    "url": "/Medicines-Order-Form-React.js/static/css/main.0c91169d.chunk.css"
+  },
+  {
+    "revision": "48d08da4970dba8a0127",
+    "url": "/Medicines-Order-Form-React.js/static/js/2.3009cde7.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/Medicines-Order-Form-React.js/static/js/2.3009cde7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8839899f0b33f337057",
+    "url": "/Medicines-Order-Form-React.js/static/js/main.2d56ef33.chunk.js"
+  },
+  {
+    "revision": "c20258cc71111cec7e6b",
+    "url": "/Medicines-Order-Form-React.js/static/js/runtime-main.a42a970c.js"
+  }
+]);
